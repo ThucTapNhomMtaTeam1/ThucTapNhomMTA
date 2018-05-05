f3f4d00eedb7684a7eabe35b574e5e6998abe510
Kien Phan <phandinhkiencntt1@gmail.com>
2018-05-05 16:04:46 +0700

    Cap Nhap UC

--- GUI/UC/UCKhoHang.Designer.cs
+++ GUI/UC/UCKhoHang.Designer.cs
@@ -28,8 +28,16 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // UCKhoHang
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Name = "UCKhoHang";
+            this.Size = new System.Drawing.Size(1068, 598);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
